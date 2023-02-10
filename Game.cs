db3f4d0b0fc4766cd1965a74db040d074db79219
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 11:34:12 +0800

    [refactor] introduce variable for substring

--- Game.cs
+++ Game.cs
@@ -5,7 +5,10 @@
         public string ShowResult(string input)
         {
             // todo: hard-cod return
-            return "White wins. - with high card: 9";
+            // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
+            var winnerPlayer = "White";
+            var winnerOutput = "9";
+            return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
         }
     }
 }
