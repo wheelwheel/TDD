7a07ecb117d3ff7f8ec4d450778aaa928948a7a4
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 10:47:11 +0800

    加入專案檔案。

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TDD
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
