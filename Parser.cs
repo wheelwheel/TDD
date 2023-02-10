0b2808a3e98b3fd45fd144f6e023df4f4eca67c4
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 14:09:15 +0800

    [feat] parse player1's cards

--- Parser.cs
+++ Parser.cs
@@ -1,4 +1,6 @@
-﻿namespace TDD
+﻿using static TDD.Parser;
+
+namespace TDD
 {
     public partial class Parser
     {
@@ -7,10 +9,23 @@
             // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
             var playerSections = input.Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
             var player1Name = playerSections[0].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
-            var player2Name = playerSections[1].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0]; 
+            var cards1 = playerSections[0].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1]
+                                          .Split(" ".ToCharArray(),StringSplitOptions.RemoveEmptyEntries)
+                                          .Select(x=>new Card()
+                                          {
+                                            Suit = x[1].ToString(),
+                                            Value = (int)char.GetNumericValue(x[0]),
+                                            Output = x[0].ToString(),
+                                          })
+                                          .ToList();
+
+            var player2Name = playerSections[1].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
 
             return new List<Player> {
-                                        new Player() { Name = player1Name },
+                                        new Player() {
+                                            Name = player1Name,
+                                            Cards = cards1
+                                        },
                                         new Player() { Name = player2Name },
                                     };
         }
