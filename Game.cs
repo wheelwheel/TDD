913fa7dcd2aed3efd7e5ec9211bcae46a5b2ad90
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 16:55:17 +0800

    [feat] compare max value card to decide winner

--- Game.cs
+++ Game.cs
@@ -8,14 +8,25 @@
             // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
             var parser = new Parser();
             var players = parser.Parse(input);
-            
-            var winnerPlayer = players[1].Name;
-            var winnerOutput = players[1]
-                               .Cards
-                               .OrderByDescending(x=>x.Value)
-                               .First()
-                               .Output;
-            return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
+
+            var maxCard1 = players[0].Cards
+                                           .OrderByDescending(x => x.Value)
+                                           .First();
+            var maxCard2 = players[1].Cards
+                                           .OrderByDescending(x => x.Value)
+                                           .First();
+
+            if (maxCard2.Value > maxCard1.Value)
+            {
+                var winnerPlayer = players[1].Name;
+                var winnerOutput = maxCard2.Output;
+                return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
+            }else
+            {
+                var winnerPlayer = players[0].Name;
+                var winnerOutput = maxCard1.Output;
+                return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
+            }
         }
     }
 }
