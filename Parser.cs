8f4786efd64a4534279ac7ee600c762135d4ecce
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 13:27:33 +0800

    [refactor to logic] parse player name

--- Parser.cs
+++ Parser.cs
@@ -4,9 +4,14 @@
     {
         public List<Player> Parse(string input)
         {
+            // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
+            var playerSections = input.Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+            var player1Name = playerSections[0].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0];
+            var player2Name = playerSections[1].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[0]; 
+
             return new List<Player> {
-                                        new Player() { Name = "Black" },
-                                        new Player() { Name = "White" },
+                                        new Player() { Name = player1Name },
+                                        new Player() { Name = player2Name },
                                     };
         }
 
