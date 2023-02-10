1ac5cf36f54b446b490a08931cddec538fac89ed
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 16:22:04 +0800

    [refactor to logic] use player2 name and max card's output for winner result

--- Game.cs
+++ Game.cs
@@ -6,8 +6,15 @@
         {
             // todo: hard-cod return
             // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
-            var winnerPlayer = "White";
-            var winnerOutput = "9";
+            var parser = new Parser();
+            var players = parser.Parse(input);
+            
+            var winnerPlayer = players[1].Name;
+            var winnerOutput = players[1]
+                               .Cards
+                               .OrderByDescending(x=>x.Value)
+                               .First()
+                               .Output;
             return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
         }
     }
