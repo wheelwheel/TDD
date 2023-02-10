bcc30ab67c367703e455ba5ecce49ae83745d6c4
wheelwheel <bigwheel1114@gmail.com>
2023-02-10 11:52:04 +0800

    [test] add failed test: "parse_player_name"

---
+++ Parser.cs
@@ -0,0 +1,15 @@
+﻿namespace TDD
+{
+    public class Parser
+    {
+        public List<Player> Parse(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public class Player
+        {
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ ParserTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TDD.Parser;
+
+namespace TDD
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        [Test]
+        public void parse_player_name()
+        {
+            var parser = new Parser();
+            // Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H
+            var players = parser.Parse("Black: 2H 3D 5S 8C 6D  White: 2C 3H 4S 9C 5H");
+            players.Should()
+                   .BeEquivalentTo(new List<Player> { 
+                                                                new Player() { Name = "Black" },
+                                                                new Player() { Name = "White" }, 
+                                                               }, options => options.WithStrictOrdering());
+        }
+    }
+}
