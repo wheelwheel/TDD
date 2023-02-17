1b99c87a3dce7129fcb8cf11105c87ba87a7c303
wheelwheel <bigwheel1114@gmail.com>
2023-02-17 17:19:54 +0800

    [refactor] split winner player and compare poker hands

--- Game.cs
+++ Game.cs
@@ -12,30 +12,25 @@
             var enumerator1 = pokerHands1.GetEnumerator();
             var enumerator2 = pokerHands2.GetEnumerator();
 
+            var compareResult = 0;
+            string winnerOutput = null;
             while (enumerator1.MoveNext() && enumerator2.MoveNext())
             {
                 var currentCard1 = enumerator1.Current;
                 var currentCard2 = enumerator2.Current;
 
-                var compareResult = currentCard1.Value - currentCard2.Value;
-
+                compareResult = currentCard1.Value - currentCard2.Value;
                 if (compareResult != 0)
                 {
-                    string winnerPlayer;
-                    string winnerOutput;
-                    if (compareResult < 0)
-                    {
-                        winnerPlayer = players[1].Name;
-                        winnerOutput = currentCard2.Output;
-                    }
-                    else
-                    {
-                        winnerPlayer = players[0].Name;
-                        winnerOutput = currentCard1.Output;
-                    }
-
-                    return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
+                    winnerOutput = compareResult < 0 ? currentCard2.Output: currentCard1.Output;
+                    break;
                 }
+            }
+
+            if (compareResult != 0)
+            {
+                var winnerPlayer = compareResult < 0 ? players[1].Name : players[0].Name;
+                return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
             }
 
             return "Tie.";
