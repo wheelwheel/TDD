09804a95089ca4de07d990441164bab43e815ba7
wheelwheel <bigwheel1114@gmail.com>
2023-02-18 11:42:41 +0800

    [feat] add Category enum

--- Game.cs
+++ Game.cs
@@ -12,10 +12,23 @@ namespace TDD
             var pokerHands2 = players[1].GetPokerHands();
 
             var pairs1 = pokerHands1.GroupBy(x => x.Value).Where(x => x.Count() == 2);
-
+            Category categoryTpye1;
             if (pairs1.Any())
             {
-                return "Black wins. - with pair: 4";
+                categoryTpye1 = Category.Pair;
+            }
+            else
+            {
+                categoryTpye1 = Category.HighCard;
+            }
+
+            if (categoryTpye1 == Category.Pair)
+            {
+                // todo: hard-code return
+                var winnerPlayer = "Black";
+                var winnerCategory = "pair";
+                var winnerOutput = "4";
+                return $"{winnerPlayer} wins. - with {winnerCategory}: {winnerOutput}";
             }
 
             var highCardComparer = new HighCardComparer();
@@ -30,5 +43,11 @@ namespace TDD
 
             return "Tie.";
         }
+    }
+
+    public enum Category
+    {
+        HighCard = 0,
+        Pair = 1,
     }
 }
