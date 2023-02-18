15cd17c6399a4717a11b81a45798e5fd5242a0e5
wheelwheel <bigwheel1114@gmail.com>
2023-02-18 16:28:28 +0800

    [refactor] move variable closer usage

--- Game.cs
+++ Game.cs
@@ -13,16 +13,16 @@ namespace TDD
 
             var pokerHands1 = players[0].GetPokerHands();
             var pokerHands2 = players[1].GetPokerHands();
-            
+
             var comparer = GetComparer(pokerHands1, pokerHands2);
 
             var compareResult = comparer.Compare(pokerHands1, pokerHands2);
-            var winnerOutput = comparer.WinnerOutput;
-            var winnerCategory = comparer.WinnerCategory;
 
             if (compareResult != 0)
             {
                 var winnerPlayer = compareResult < 0 ? players[1].Name : players[0].Name;
+                var winnerOutput = comparer.WinnerOutput;
+                var winnerCategory = comparer.WinnerCategory;
                 return $"{winnerPlayer} wins. - with {winnerCategory}: {winnerOutput}";
             }
 
@@ -31,17 +31,12 @@ namespace TDD
 
         private static IPokerHandsComparer GetComparer(PokerHands pokerHands1, PokerHands pokerHands2)
         {
-            IPokerHandsComparer comparer;
             if (pokerHands1.GetCategory().Type != pokerHands2.GetCategory().Type)
             {
-                comparer = new DifferentCategoryComparer();
-            }
-            else
-            {
-                comparer = new HighCardComparer();
+                return new DifferentCategoryComparer();
             }
 
-            return comparer;
+            return new HighCardComparer();
         }
     }
 }
