a38098960a1e528f54a1df1878f2b0e65304b127
wheelwheel <bigwheel1114@gmail.com>
2023-02-18 08:54:35 +0800

    [refactor] extract method: Compare()

--- Game.cs
+++ Game.cs
@@ -9,23 +9,7 @@
             var pokerHands1 = players[0].GetPokerHands();
             var pokerHands2 = players[1].GetPokerHands();
 
-            var enumerator1 = pokerHands1.GetEnumerator();
-            var enumerator2 = pokerHands2.GetEnumerator();
-
-            var compareResult = 0;
-            string winnerOutput = null;
-            while (enumerator1.MoveNext() && enumerator2.MoveNext())
-            {
-                var currentCard1 = enumerator1.Current;
-                var currentCard2 = enumerator2.Current;
-
-                compareResult = currentCard1.Value - currentCard2.Value;
-                if (compareResult != 0)
-                {
-                    winnerOutput = compareResult < 0 ? currentCard2.Output: currentCard1.Output;
-                    break;
-                }
-            }
+            var compareResult = Compare(pokerHands1, pokerHands2, out var winnerOutput);
 
             if (compareResult != 0)
             {
@@ -34,6 +18,29 @@
             }
 
             return "Tie.";
+        }
+
+        private static int Compare(IEnumerable<Card> pokerHands1, IEnumerable<Card> pokerHands2, out string winnerOutput)
+        {
+            var enumerator1 = pokerHands1.GetEnumerator();
+            var enumerator2 = pokerHands2.GetEnumerator();
+
+            var compareResult = 0;
+            winnerOutput = null;
+            while (enumerator1.MoveNext() && enumerator2.MoveNext())
+            {
+                var currentCard1 = enumerator1.Current;
+                var currentCard2 = enumerator2.Current;
+
+                compareResult = currentCard1.Value - currentCard2.Value;
+                if (compareResult != 0)
+                {
+                    winnerOutput = compareResult < 0 ? currentCard2.Output : currentCard1.Output;
+                    break;
+                }
+            }
+
+            return compareResult;
         }
     }
 }
