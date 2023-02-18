0cc9da982050dbc19e6ee69ba950841b9771a1a7
wheelwheel <bigwheel1114@gmail.com>
2023-02-18 09:21:24 +0800

    [refactor] exrtract class: HighCardComparer from Compare()

--- Game.cs
+++ Game.cs
@@ -1,26 +1,8 @@
 ﻿namespace TDD
 {
-    public class Game
+    public class HighCardComparer
     {
-        public string ShowResult(string input)
-        {
-            var players = new Parser().Parse(input);
-
-            var pokerHands1 = players[0].GetPokerHands();
-            var pokerHands2 = players[1].GetPokerHands();
-
-            var compareResult = Compare(pokerHands1, pokerHands2, out var winnerOutput);
-
-            if (compareResult != 0)
-            {
-                var winnerPlayer = compareResult < 0 ? players[1].Name : players[0].Name;
-                return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
-            }
-
-            return "Tie.";
-        }
-
-        private static int Compare(IEnumerable<Card> pokerHands1, IEnumerable<Card> pokerHands2, out string winnerOutput)
+        public int Compare(IEnumerable<Card> pokerHands1, IEnumerable<Card> pokerHands2, out string winnerOutput)
         {
             var enumerator1 = pokerHands1.GetEnumerator();
             var enumerator2 = pokerHands2.GetEnumerator();
@@ -41,6 +23,29 @@
             }
 
             return compareResult;
+        }
+    }
+
+    public class Game 
+    {
+        private readonly HighCardComparer _highCardComparer = new HighCardComparer();
+
+        public string ShowResult(string input)
+        {
+            var players = new Parser().Parse(input);
+
+            var pokerHands1 = players[0].GetPokerHands();
+            var pokerHands2 = players[1].GetPokerHands();
+
+            var compareResult = _highCardComparer.Compare(pokerHands1, pokerHands2, out var winnerOutput);
+
+            if (compareResult != 0)
+            {
+                var winnerPlayer = compareResult < 0 ? players[1].Name : players[0].Name;
+                return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
+            }
+
+            return "Tie.";
         }
     }
 }
