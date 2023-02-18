03df18b7405793413d48f5d57d2f88a87ea39adf
wheelwheel <bigwheel1114@gmail.com>
2023-02-18 10:10:24 +0800

    [refactor] Use WinnerOutput property, instead of out parmeter

---
+++ Comparers/HighCardComparer.cs
@@ -0,0 +1,29 @@
+﻿namespace TDD.Comparers
+{
+    public class HighCardComparer
+    {
+        public string WinnerOutput { get; private set; }
+
+        public int Compare(IEnumerable<Card> pokerHands1, IEnumerable<Card> pokerHands2)
+        {
+            var enumerator1 = pokerHands1.GetEnumerator();
+            var enumerator2 = pokerHands2.GetEnumerator();
+
+            var compareResult = 0;
+            while (enumerator1.MoveNext() && enumerator2.MoveNext())
+            {
+                var currentCard1 = enumerator1.Current;
+                var currentCard2 = enumerator2.Current;
+
+                compareResult = currentCard1.Value - currentCard2.Value;
+                if (compareResult != 0)
+                {
+                    WinnerOutput = compareResult < 0 ? currentCard2.Output : currentCard1.Output;
+                    break;
+                }
+            }
+
+            return compareResult;
+        }
+    }
+}
--- Game.cs
+++ Game.cs
@@ -1,35 +1,9 @@
-﻿namespace TDD
+﻿using TDD.Comparers;
+
+namespace TDD
 {
-    public class HighCardComparer
-    {
-        public int Compare(IEnumerable<Card> pokerHands1, IEnumerable<Card> pokerHands2, out string winnerOutput)
-        {
-            var enumerator1 = pokerHands1.GetEnumerator();
-            var enumerator2 = pokerHands2.GetEnumerator();
-
-            var compareResult = 0;
-            winnerOutput = null;
-            while (enumerator1.MoveNext() && enumerator2.MoveNext())
-            {
-                var currentCard1 = enumerator1.Current;
-                var currentCard2 = enumerator2.Current;
-
-                compareResult = currentCard1.Value - currentCard2.Value;
-                if (compareResult != 0)
-                {
-                    winnerOutput = compareResult < 0 ? currentCard2.Output : currentCard1.Output;
-                    break;
-                }
-            }
-
-            return compareResult;
-        }
-    }
-
     public class Game 
     {
-        private readonly HighCardComparer _highCardComparer = new HighCardComparer();
-
         public string ShowResult(string input)
         {
             var players = new Parser().Parse(input);
@@ -37,11 +11,13 @@
             var pokerHands1 = players[0].GetPokerHands();
             var pokerHands2 = players[1].GetPokerHands();
 
-            var compareResult = _highCardComparer.Compare(pokerHands1, pokerHands2, out var winnerOutput);
-
+            var highCardComparer = new HighCardComparer();
+            var compareResult = highCardComparer.Compare(pokerHands1, pokerHands2);
+           
             if (compareResult != 0)
             {
                 var winnerPlayer = compareResult < 0 ? players[1].Name : players[0].Name;
+                var winnerOutput = highCardComparer.WinnerOutput;
                 return $"{winnerPlayer} wins. - with high card: {winnerOutput}";
             }
 
