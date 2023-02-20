23f8487d98b67aa2a5b8ea1243e4a85478d491a2
wheelwheel <bigwheel1114@gmail.com>
2023-02-20 09:10:22 +0800

    [refactor] extract method: GetPairs()

--- Comparers/PairCpmaprer.cs
+++ Comparers/PairCpmaprer.cs
@@ -7,13 +7,18 @@
 
         public int Compare(PokerHands pokerHands1, PokerHands pokerHands2)
         {
-            var pairs1 = pokerHands1.GroupBy(x => x.Value).Where(x => x.Count() == 2);
+            IEnumerable<IGrouping<int, Card>> pairs1 = GetPairs(pokerHands1);
             var pairs2 = pokerHands2.GroupBy(x => x.Value).Where(x => x.Count() == 2);
 
             var compareResult = pairs1.First().First().Value - pairs2.First().First().Value;
             WinnerOutput = compareResult > 0 ? pairs1.First().First().Output : pairs2.First().First().Output;
 
             return compareResult;
+        }
+
+        private static IEnumerable<IGrouping<int, Card>> GetPairs(PokerHands pokerHands)
+        {
+            return pokerHands.GroupBy(x => x.Value).Where(x => x.Count() == 2);
         }
     }
 }
