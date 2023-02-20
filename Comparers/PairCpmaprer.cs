1e8d8a2c641d56a92cd68344b2c04e1c6c4e0146
wheelwheel <bigwheel1114@gmail.com>
2023-02-20 10:41:49 +0800

    [refactor] extract method: GetFirstCardOfEachGroup()

--- Comparers/PairCpmaprer.cs
+++ Comparers/PairCpmaprer.cs
@@ -4,13 +4,10 @@
     {
         public string WinnerOutput { get; private set; }
         public string WinnerCategory => "pair";
-        
+
         public int Compare(PokerHands pokerHands1, PokerHands pokerHands2)
         {
-            var firstCardOfEachGroup1 = pokerHands1.GroupBy(x => x.Value)
-                                                   .OrderByDescending(x => x.Count())
-                                                   .Select(x => x.First());
-
+            var firstCardOfEachGroup1 = GetFirstCardOfEachGroup(pokerHands1);
             var firstCardOfEachGroup2 = pokerHands2.GroupBy(x => x.Value)
                                                    .OrderByDescending(x => x.Count())
                                                    .Select(x => x.First());
@@ -18,14 +15,15 @@
             var highCardComparer = new HighCardComparer();
             var compareResult = highCardComparer.CompareCardsByValue(firstCardOfEachGroup1, firstCardOfEachGroup2);
             WinnerOutput = highCardComparer.WinnerOutput;
-            //var pairs1 = pokerHands1.GetPairs();
-            //var pairs2 = pokerHands2.GetPairs();
-            //
-            //var compareResult = pairs1.First().First().Value - pairs2.First().First().Value;
-            //WinnerOutput = compareResult > 0 ? pairs1.First().First().Output : pairs2.First().First().Output;
-
 
             return compareResult;
+        }
+
+        private static IEnumerable<Card> GetFirstCardOfEachGroup(PokerHands pokerHands)
+        {
+            return pokerHands.GroupBy(x => x.Value)
+                             .OrderByDescending(x => x.Count())
+                             .Select(x => x.First());
         }
     }
 }
