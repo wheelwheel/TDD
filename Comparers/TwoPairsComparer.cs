89b82945bbfe1a17056366c5d29afd3bf20291f7
wheelwheel <bigwheel1114@gmail.com>
2023-02-23 11:56:44 +0800

    clean up

---
+++ Comparers/TwoPairsComparer.cs
@@ -0,0 +1,12 @@
+﻿namespace TDD.Comparers
+{
+    internal class TwoPairsComparer : SameCategoryComparer
+    {
+        public override string WinnerCategory => "two pairs";
+
+        public override int Compare(PokerHands pokerHands1, PokerHands pokerHands2)
+        {
+            return CompareCardsByValue(pokerHands1.GetFirstCardOfEachGroup(), pokerHands2.GetFirstCardOfEachGroup());
+        }
+    }
+}
--- Game.cs
+++ Game.cs
@@ -48,14 +48,4 @@ namespace TDD
             return SameComparersLookup[categoryType1];
         }
     }
-
-    internal class TwoPairsComparer : SameCategoryComparer
-    {
-        public override string WinnerCategory => "two pairs";
-
-        public override int Compare(PokerHands pokerHands1, PokerHands pokerHands2)
-        {
-            return CompareCardsByValue(pokerHands1.GetFirstCardOfEachGroup(), pokerHands2.GetFirstCardOfEachGroup());
-        }
-    }
 }
